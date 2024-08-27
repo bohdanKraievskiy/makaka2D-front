e96c5044723a1a29a4cb1455be7c48a85ffcce5e
bohdan_k <118996991+bohdanKraievskiy@users.noreply.github.com>
2024-08-27 12:35:59 +0300

    Update loading_page.js

--- src/pages/loading_page.js
+++ src/pages/loading_page.js
@@ -45,6 +45,7 @@ const PreLoad = ({ telegramId }) => {
         loadData();
     }, [telegramId, navigate]);
     const fetchAllData = async (telegramId) => {
+        await fetchUser(telegramId);
         await fetchUserRewards(telegramId);
         await fetchTasks(telegramId);
     };
